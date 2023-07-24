30f6780c8fc832629af02d80d5eccde0e7d6582f
alhaj <alhaj.s@theextraaedge.com>
2023-07-24 12:30:51 +0530

    added code for table 2

--- lib/src/helper/pluto_row_group_delegate.dart
+++ lib/src/helper/pluto_row_group_delegate.dart
@@ -550,11 +550,7 @@ class PlutoRowGroupByColumnDelegate extends PlutoRowGroupDelegate {
     for (var e in sampleRow.first.cells.entries) {
       final column = visibleColumns.firstWhereOrNull((c) => c.field == e.key);
       cells[e.key] = PlutoCell(
-          value: column == null
-              ? null
-              : column.type == PlutoColumnType.number()
-                  ? hashmapOfData?[e.key]?['count'] ?? 0
-                  : e.value.value,
+          value: 69,
           key: ValueKey('${groupKey}_${e.key}_cell'),
           data: {
             'key': key,
